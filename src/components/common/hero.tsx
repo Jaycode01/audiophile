0ea5d01e326eb841b1bd1254eca619578c486c92
Jaycode01 <josephlamidi01@gmail.com>
2025-10-31 12:48:27 +0100

    milestone two: create navbar +_ hero section [with responsiveness]

--- src/components/common/hero.tsx
+++ src/components/common/hero.tsx
@@ -1,7 +1,22 @@
 import "./hero.css";
 
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="hero">
+      <div className="text_content">
+        <p className="label">New Product</p>
+        <h1 className="heading">XX99 Mark II HeadphoneS</h1>
+        <p className="description">
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+        <button type="button">See Product</button>
+      </div>
+      <div className="image_content">
+        <img src="/assets/hero_img.png" alt="hero image" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
