794b091b36256c97c700b938768b5cc1045e97db
Jaycode01 <josephlamidi01@gmail.com>
2025-10-31 17:43:38 +0100

    createthe component to show off the first speaker named zx9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Category from "@/components/common/category";
 import Hero from "@/components/common/hero";
+import Zx9Speaker from "@/components/common/zx9speaker";
 import Navbar from "@/components/layout/navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Category />
+      <Zx9Speaker />
     </>
   );
 }
