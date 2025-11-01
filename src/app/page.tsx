eb8210131cad2ec06f8bde6eb978f5545015e1ad
Jaycode01 <josephlamidi01@gmail.com>
2025-11-01 14:38:36 +0100

    i studeid and understand what really pixel-perfect is and also get to update other sections and create the section to display the yx1earphone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Category from "@/components/common/category";
 import Hero from "@/components/common/hero";
+import Yx1earphone from "@/components/common/yx1earphone";
 import Zx7speaker from "@/components/common/zx7speaker";
 import Zx9Speaker from "@/components/common/zx9speaker";
 import Navbar from "@/components/layout/navbar";
@@ -12,6 +13,7 @@ function App() {
       <Category />
       <Zx9Speaker />
       <Zx7speaker />
+      <Yx1earphone />
     </>
   );
 }
---
+++ src/components/common/yx1earphone.tsx
@@ -0,0 +1,14 @@
+import "./yx1earphone.css";
+
+const Yx1earphone = () => {
+  return (
+    <section className="yx1_display">
+      <img src="/assets/yx1earphone.jpg" alt="yx1earphone" />
+      <div className="yx1_cta">
+        <h2 className="heading">Yx1 Earphones</h2>
+        <button type="button">See Product</button>
+      </div>
+    </section>
+  );
+};
+export default Yx1earphone;
