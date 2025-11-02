71d43677ed0ba5dbb104ab790ba7fc73310883e9
Jaycode01 <josephlamidi01@gmail.com>
2025-11-02 23:49:00 +0100

    done - milestone 8: create earphones category page

--- src/app/earphones/page.tsx
+++ src/app/earphones/page.tsx
@@ -1,5 +1,27 @@
+import About from "@/components/common/about";
+import Category from "@/components/common/category";
+import CategoryHero from "@/components/common/category_hero";
+import ProductShowcase from "@/components/common/showcase";
+import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/navbar";
+
 function Earphones() {
-  return <>Earphones</>;
+  return (
+    <>
+      <Navbar />
+      <CategoryHero category_name="Earphones" />
+      <ProductShowcase
+        label="New Product"
+        desktop_image="/assets/earphones/yx1.png"
+        title="YX1 WIRELESS EARPHONES"
+        description="Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature."
+        reverse={false}
+      />
+      <Category />
+      <About />
+      <Footer />
+    </>
+  );
 }
 
 export default Earphones;
